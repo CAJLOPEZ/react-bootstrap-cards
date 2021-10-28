184a4b74401ef610263af069beedc5f58741bb99
Camila Lopez <camila.lopez.91@hotmail.com>
2021-10-28 14:39:32 -0300

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="" alt=""/>
+            <div className="card-body">
+                <h4 className="card-title">My title</h4>
+                <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Ipsum numquam quaerat aut voluptatem, repellat dolores iure asperiores porro, incidunt labore perspiciatis, 
+                vitae saepe sit. Eos ratione iste suscipit quisquam veniam.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
