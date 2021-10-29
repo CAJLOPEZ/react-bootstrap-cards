38da5c418af02d2c46882aef8cb262624fd09166
Camila Lopez <camila.lopez.91@hotmail.com>
2021-10-29 18:26:58 -0300

    third commit - animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import './cards.css'
 
-function Card({title, imageSource, url}) {
+function Card({title, imageSource, url, text}) {
     return (
-        <div className="card text-center bg-dark">
-            <img src={imageSource} alt=""/>
+        <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+            <div className="overflow">
+               <img src={imageSource} alt="" className="card-img-top"/>
+            </div>
             <div className="card-body text-light">
                 <h4 className="card-title">{title}</h4>
-                <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Ipsum numquam quaerat aut voluptatem, repellat dolores iure asperiores porro, incidunt labore perspiciatis, 
-                vitae saepe sit. Eos ratione iste suscipit quisquam veniam.</p>
+                <p className="card-text text-secondary">
+                {
+                    text ? text : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum numquam quaerat aut voluptatem, repellat dolores iure asperiores porro, incidunt labore perspiciatis, vitae saepe sit. Eos ratione iste suscipit quisquam veniam.'
+                }
+                </p>
                 <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank">
                     Go to this website 
                 </a>
             </div>
         </div>
     )
+}
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string,
+    image: PropTypes.string,
+    text: PropTypes.string
 }
 
 export default Card
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,19 +7,20 @@ import image3 from '../assets/image3.jpg'
 const cards = [
     {
         id: 1,
-        title: 'Linkedin',
+        title: 'Kamy Linkedin',
         image: image1,
-        url: 'https://www.linkedin.com/in/camila-jlopez/'
+        url: 'https://www.linkedin.com/in/camila-jlopez/',
+        text: 'Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.'
     },
     {
         id: 2,
-        title: 'Github',
+        title: 'Kamy Github',
         image: image2,
         url: 'https://github.com/CAJLOPEZ'
     },
     {
         id: 3,
-        title: 'Instagram',
+        title: 'Kamy Instagram',
         image: image3,
         url: 'https://instagram.com/mrskamylee'
     },
@@ -32,7 +33,7 @@ function Cards(){
                 {
                     cards.map((card) => (
                         <div className="col-md-4" key={card.id}> 
-                            <Card title={card.title} imageSource={card.image} url={card.url}/>
+                            <Card title={card.title} imageSource={card.image} url={card.url} text={card.text}/>
                         </div>
                     ))
                 }
